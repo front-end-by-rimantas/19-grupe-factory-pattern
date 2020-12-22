77a5d1527474c119890d108dde009b84f06f56c8
Rimantas <belauzas@yahoo.com>
2020-12-22 16:07:03 +0200

    No action bug fix

--- main.js
+++ main.js
@@ -21,10 +21,14 @@ const calc = text => {
     let parts = text.split(' ').map((part, i) => i % 2 === 0 ? parseInt(part) : part);
     const action = actions[parts[1]];
 
+    if (!action) {
+        return 'ERROR: no valid action.';
+    }
+
     return action(parts[0], parts[2]);
 };
 
 
-let userInput = '13 padauginti 17';
+let userInput = '13 minus 17';
 const rez = calc(userInput);
 console.log(userInput, '=', rez);
