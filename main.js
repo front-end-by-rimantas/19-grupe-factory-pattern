55985952645cd0f577f910bf55baad35e8eb2fb0
Rimantas <belauzas@yahoo.com>
2020-12-22 16:01:06 +0200

    Skaiciuojame interpretuota matematini teksta

---
+++ main.js
@@ -0,0 +1,30 @@
+const sum = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const calc = text => {
+    const actions = {
+        '+': sum,
+        'plus': sum,
+        'prideti': sum,
+        '-': minus,
+        'minus': minus,
+        'atimti': minus,
+        '*': multiply,
+        'multiply': multiply,
+        'padauginti': multiply,
+        '/': divide,
+        'divide': divide,
+        'padalinti': divide
+    };
+    let parts = text.split(' ').map((part, i) => i % 2 === 0 ? parseInt(part) : part);
+    const action = actions[parts[1]];
+
+    return action(parts[0], parts[2]);
+};
+
+
+let userInput = '13 padauginti 17';
+const rez = calc(userInput);
+console.log(userInput, '=', rez);
